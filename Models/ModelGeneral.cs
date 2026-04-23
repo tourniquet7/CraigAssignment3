63af888609f7d9bcf3a3ca49ca91c57606d91e21
cnewland <craig.d.newland@gmail.com>
2026-04-22 22:55:31 -0400

    add

---
+++ Models/ModelGeneral.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FISSystem.Models
+{
+    public class Transaction
+    {
+        public string TransactionID { get; set; }
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
